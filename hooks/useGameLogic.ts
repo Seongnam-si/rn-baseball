01b2225ba99a68fc1d8d61f76213d62ed3d78c66
Seongnam Hong <hongdev26@gmail.com>
2025-12-16 04:10:21 +0900

    Feat : 키패드 입력 및 입력수 삭제 관련 로직 작성

--- hooks/useGameLogic.ts
+++ hooks/useGameLogic.ts
@@ -7,6 +7,9 @@ const useGameLogic = () => {
   const [numLength, setNumLength] = useState<number>(0);
   const [comNumber, setComNumber] = useState<number[]>([]);
   const [attempts, setAttempts] = useState([]);
+  const [inputNumber, setInputNumber] = useState<number[]>([]);
+  const [isJudgeTrigger, setIsJudgeTrigger] = useState<boolean>(false);
+  const [isCheckDone, setIsCheckDone] = useState<boolean>(false);
 
   useEffect(() => {
     if (gameMode) {
@@ -19,9 +22,25 @@ const useGameLogic = () => {
       }
     }
   }, [gameMode]);
+
+  const handleClickDeleteNumber = () => {
+    setInputNumber(prevNum => (
+      prevNum.length > 0 ? prevNum.slice(0, -1) : prevNum
+    ));
+  };
+
+  const handleClickNumber = (num: number) => {
+    setInputNumber(prevNum =>
+      ((prevNum.length < numLength) && (!prevNum.includes(num))) ?
+        [...prevNum, num] :
+        prevNum
+    );
+  };
   
   return {
-    isModalOpen, setIsModalOpen, setGameMode, attempts
+    isModalOpen, setIsModalOpen, setGameMode, attempts,
+    inputNumber, setIsJudgeTrigger, numLength, isCheckDone, handleClickDeleteNumber,
+    handleClickNumber
   }
 };
 
