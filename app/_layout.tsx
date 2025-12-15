23233e8170c688b87545ba3875c9690041b3cad5
Seongnam Hong <hongdev26@gmail.com>
2025-12-16 01:58:08 +0900

    Feat : 노치관련 옵션 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Start Page" }} />
-      <Stack.Screen name="mainPage" options={{ title : "Main Page" }}/>
-    </Stack>
+    <SafeAreaView style={{ flex: 1 }} edges={["top", "left", "right"]}>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Start Page" }} />
+        <Stack.Screen name="mainPage" options={{ headerShown : false }}/>
+      </Stack>
+    </SafeAreaView>
   );
 }
