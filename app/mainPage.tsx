8dd24bf3385fd49bcb8205baa50176ef78b4c28c
Seongnam Hong <hongdev26@gmail.com>
2025-12-18 11:49:08 +0900

    Feat : mainPage 컴포넌트에 관련 컴포넌트들 적용

--- app/mainPage.tsx
+++ app/mainPage.tsx
@@ -1,17 +1,45 @@
-import IntroModal from "@/components/modal";
+import Display from "@/components/Display";
+import DisplayBanner from "@/components/DisplayBanner";
+import InputWindow from "@/components/InputWindow";
+import IntroModal from "@/components/IntroModal";
+import Keypad from "@/components/Keypad";
 import useGameLogic from "@/hooks/useGameLogic";
+import { View } from "react-native";
 
 export default function MainPage() {
   const {
-    isModalOpen, setIsModalOpen, setGameMode
+    isModalOpen, setGameMode, gameState, resetGame, playExtraInning,
+    attempts,
+    inputNumber, setIsJudgeTrigger, handleClickDeleteNumber, numLength, isCheckDone,
+    handleClickNumber, 
   } = useGameLogic();
 
   return (
-    <>
+    <View className="flex-1">
       <IntroModal
         modalState={isModalOpen}
         gameModeSetter={setGameMode}
+        gameState={gameState}
+        onRestart={resetGame}
+        playExtraInning={playExtraInning}
       />
-    </>
+      <Display
+        attempts={attempts}
+      />
+      <DisplayBanner
+        modalState={isModalOpen}
+        attempts={attempts}
+      />
+      <InputWindow
+        userNumber={inputNumber}
+        judgeTriggerSetter={() => setIsJudgeTrigger(true)}
+        deleteNumberSetter={handleClickDeleteNumber}
+        numLength={numLength}
+        enterActivate={isCheckDone}
+      />
+      <Keypad
+        numberSetter={handleClickNumber}
+      />
+    </View>
   );
 };
