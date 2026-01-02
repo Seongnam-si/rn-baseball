a552843a84b8e2e37221159e1815828833aaab73
Seongnam Hong <hongdev26@gmail.com>
2026-01-03 08:30:02 +0900

    Feat : 시작페이지 통계 모달창 적용

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
+import GameStatsModal from "@/components/GameStatsModal";
 import { router } from "expo-router";
+import { useState } from "react";
 import { Image, Pressable, Text, View } from "react-native";
 
 export default function Index() {
+  const [isShowStats, setIsShowStats] = useState<boolean>(false);
 
   const handleTouch = () => {
     router.push("./mainPage");
@@ -17,7 +20,7 @@ export default function Index() {
         숫자야구
       </Text>
       <Text className="font-semibold text-2xl text-gray-600 mt-6">
-        숫자를 추리하고 홈런을 날리세요 ⚾️
+        숫자를 추리하고 홈런을 날리세요
       </Text>
       <Pressable 
         className="rounded-full w-40 h-20 bg-[#0064FF] items-center justify-center mt-10"
@@ -27,6 +30,15 @@ export default function Index() {
           게임 시작
         </Text>
       </Pressable>
+      <Pressable
+        className="rounded-full w-40 h-20 bg-[#6B7280] items-center justify-center mt-6"
+        onPress={() => setIsShowStats(true)}
+      >
+        <Text className="font-bold text-2xl text-white">
+          통계 보기
+        </Text>
+      </Pressable>
+      <GameStatsModal isVisible={isShowStats} onClose={() => setIsShowStats(false)} />
     </View>
   );
 }
